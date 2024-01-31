b5f5473107b469b00405eae4aaa612b51c791ca6
crazymax777m <maximus777m@mail.ru>
2024-01-31 22:26:46 +0300

    PUT hw18-multiprocessApplications ADD config.properties FIX load balancer logic ADD try-with-resources

--- hw18-multiprocessApplications/src/main/java/LoadBalancer.java
+++ hw18-multiprocessApplications/src/main/java/LoadBalancer.java
@@ -3,87 +3,98 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.Properties;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class LoadBalancer {
     private static final List<NodeInfo> nodes = new ArrayList<>();
 
     public static void main(String[] args) {
-        // Запуск сервера LoadBalancer на порту 8080 для регистрации Node
+
+        Properties properties = new Properties();
+
+        try {
+            properties.load(new FileReader("hw18-multiprocessApplications/src/main/java/config.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        int registrationPort = Integer.parseInt(properties.getProperty("registration.port"));
+        int userRequestsPort = Integer.parseInt(properties.getProperty("user.requests.port"));
+        int fixedThreadsCount = Integer.parseInt(properties.getProperty("fixed.threads.count"));
+
+        // Запуск сервера LoadBalancer на порту registrationPort для регистрации Node
         new Thread(() -> {
-            try {
-                ServerSocket registrationSocket = new ServerSocket(8080);
+            try (ServerSocket registrationSocket = new ServerSocket(registrationPort)) {
                 while (true) {
                     Socket nodeSocket = registrationSocket.accept();
-                    BufferedReader registerInput = new BufferedReader(new InputStreamReader(nodeSocket.getInputStream()));
-                    String registerCommand = registerInput.readLine();
-                    if ("REGISTER".equals(registerCommand)) {
-                        int nodePort = Integer.parseInt(registerInput.readLine());
-                        NodeInfo nodeInfo = new NodeInfo(nodeSocket.getInetAddress().getHostAddress(), nodePort);
-                        nodes.add(nodeInfo);
-                        System.out.println("Node registered: " + nodeInfo);
+                    try (BufferedReader registerInput = new BufferedReader(new InputStreamReader(nodeSocket.getInputStream()))) {
+                        String registerCommand = registerInput.readLine();
+                        if ("REGISTER".equals(registerCommand)) {
+                            int nodePort = Integer.parseInt(registerInput.readLine());
+                            NodeInfo nodeInfo = new NodeInfo(nodeSocket.getInetAddress().getHostAddress(), nodePort);
+                            nodes.add(nodeInfo);
+                            System.out.println("Node registered: " + nodeInfo);
+                        }
                     }
-                    registerInput.close();
-                    nodeSocket.close();
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }).start();
 
-        // Запуск сервера LoadBalancer на порту 9091 для обработки запросов от пользователей
-        new Thread(() -> {
-            try {
-                ServerSocket serverSocket = new ServerSocket(9091);
-                while (true) {
-                    Socket clientSocket = serverSocket.accept();
-                    BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                    String request = input.readLine();
-                    String response = processRequest(request);
-                    PrintWriter output = new PrintWriter(clientSocket.getOutputStream(), true);
-                    output.println(response);
-                    output.close();
-                    input.close();
-                    clientSocket.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }).start();
+        ExecutorService executorService = Executors.newFixedThreadPool(fixedThreadsCount);
 
-        startNode(9092);
-        startNode(9093);
-        startNode(9094);
-    }
-
-    private static void startNode(int port) {
-        new Thread(() -> {
-            try {
-                ProcessBuilder processBuilder = new ProcessBuilder("java", "Node", String.valueOf(port));
-                processBuilder.start();
-            } catch (IOException e) {
-                e.printStackTrace();
+        // Запуск сервера LoadBalancer на порту userRequestsPort для обработки запросов от пользователей
+        try (ServerSocket serverSocket = new ServerSocket(userRequestsPort)) {
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                executorService.submit(() -> {
+                    try (
+                            BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                            PrintWriter output = new PrintWriter(clientSocket.getOutputStream(), true)
+                    ) {
+                        String request = input.readLine();
+                        String response = processRequest(request);
+                        output.println(response);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
             }
-        }).start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     private static String processRequest(String request) {
         StringBuilder response = new StringBuilder();
-        for (NodeInfo node : nodes) {
-            try {
-                Socket nodeSocket = new Socket(node.getHost(), node.getPort());
-                PrintWriter nodeOutput = new PrintWriter(nodeSocket.getOutputStream(), true);
-                nodeOutput.println(request);
-                BufferedReader nodeInput = new BufferedReader(new InputStreamReader(nodeSocket.getInputStream()));
-                String nodeResponse = nodeInput.readLine();
-                response.append(nodeResponse).append("\n");
-                nodeInput.close();
-                nodeOutput.close();
-                nodeSocket.close();
-            } catch (IOException e) {
-                System.err.println("Node " + node + " did not respond.");
-            }
+
+        int nodesSize = nodes.size();
+
+        if (nodesSize == 0) {
+            return "No available nodes";
         }
+
+        // Случайным образом выбираем индекс ноды
+        int randomNodeIndex = ThreadLocalRandom.current().nextInt(nodesSize);
+        NodeInfo selectedNode = nodes.get(randomNodeIndex);
+
+        try (
+                Socket nodeSocket = new Socket(selectedNode.getHost(), selectedNode.getPort());
+                PrintWriter nodeOutput = new PrintWriter(nodeSocket.getOutputStream(), true);
+                BufferedReader nodeInput = new BufferedReader(new InputStreamReader(nodeSocket.getInputStream()))
+        ) {
+            nodeOutput.println(request);
+            String nodeResponse = nodeInput.readLine();
+            response.append(nodeResponse).append("\n");
+        } catch (IOException e) {
+            System.err.println("Node " + selectedNode + " did not respond.");
+        }
+
         return response.toString().trim();
     }
 
--- hw18-multiprocessApplications/src/main/java/Node.java
+++ hw18-multiprocessApplications/src/main/java/Node.java
@@ -1,43 +1,72 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Properties;
 
 public class Node {
     public static void main(String[] args) {
-        int port = Integer.parseInt(args[0]); // Передаем порт как аргумент командной строки
+        int port = getPortFromArgs(args);
+
+        if (port == -1) {
+            System.err.println("Invalid input. Please provide a valid integer port number.");
+            return;
+        }
+
         registerNode(port);
         handleRequests(port);
     }
 
-    private static void registerNode(int port) {
+    private static int getPortFromArgs(String[] args) {
         try {
-            Socket registerSocket = new Socket("localhost", 8080);
-            PrintWriter registerOutput = new PrintWriter(registerSocket.getOutputStream(), true);
+            return Integer.parseInt(args[0]);
+        } catch (ArrayIndexOutOfBoundsException | NumberFormatException e) {
+            return -1;
+        }
+    }
+
+    private static void registerNode(int port) {
+        int registrationPort = getRegistrationPortFromConfig();
+        if (registrationPort == -1) {
+            System.err.println("Error reading registration port from config.");
+            return;
+        }
+
+        try (Socket registerSocket = new Socket("localhost", registrationPort);
+             PrintWriter registerOutput = new PrintWriter(registerSocket.getOutputStream(), true)) {
             registerOutput.println("REGISTER");
             registerOutput.println(port);
-            registerOutput.close();
-            registerSocket.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     private static void handleRequests(int port) {
-        try {
-            ServerSocket serverSocket = new ServerSocket(port);
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
             while (true) {
-                Socket clientSocket = serverSocket.accept();
-                BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                String request = input.readLine();
-                String response = "Hello from Node on port " + port;
-                PrintWriter output = new PrintWriter(clientSocket.getOutputStream(), true);
-                output.println(response);
-                output.close();
-                input.close();
-                clientSocket.close();
+                try (
+                        Socket clientSocket = serverSocket.accept();
+                        BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                        PrintWriter output = new PrintWriter(clientSocket.getOutputStream(), true)
+                ) {
+                    String request = input.readLine();
+                    String response = "Hello from Node on port " + port;
+                    output.println(response);
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    private static int getRegistrationPortFromConfig() {
+        Properties properties = new Properties();
+        try {
+            properties.load(new FileReader("config.properties"));
+            return Integer.parseInt(properties.getProperty("registration.port"));
+        } catch (IOException | NumberFormatException e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+
 }
